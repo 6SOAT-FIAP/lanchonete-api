cc6e4e08170fb3893a02b0cf20ea8dc58f38ad2a
Luhan Lacerda <luhanlacerda@gmail.com>
2024-05-18 14:38:52 -0300

    merge main

---
+++ src/main/java/pos/fiap/lanchonete/adapter/config/MongoConfig.java
@@ -0,0 +1,15 @@
+package pos.fiap.lanchonete.adapter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoClientFactoryBean;
+
+@Configuration
+public class MongoConfig {
+
+    public @Bean MongoClientFactoryBean mongo() {
+        MongoClientFactoryBean mongo = new MongoClientFactoryBean();
+        mongo.setHost("localhost");
+        return mongo;
+    }
+}
