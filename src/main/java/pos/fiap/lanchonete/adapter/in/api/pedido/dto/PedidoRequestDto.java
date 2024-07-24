a2543fbd8a502415a2281ce90173c531e058991b
stephaniesantosss <stephanieps2016@hotmail.com>
2024-07-24 11:24:45 -0300

    feature: atualizacao status pedido

--- src/main/java/pos/fiap/lanchonete/adapter/in/api/pedido/dto/PedidoRequestDto.java
+++ src/main/java/pos/fiap/lanchonete/adapter/in/api/pedido/dto/PedidoRequestDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 import pos.fiap.lanchonete.adapter.in.api.produto.dto.ProdutoRequestDto;
+import pos.fiap.lanchonete.domain.enums.StatusPedidoEnum;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -18,5 +19,6 @@ public class PedidoRequestDto implements Serializable {
     @Serial
     private static final long serialVersionUID = -2674614054267148943L;
     private String cpfCliente;
+    private StatusPedidoEnum statusPedido;
     private List<ProdutoRequestDto> itens;
 }
