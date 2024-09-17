ba2336212b2fd07d68adca7b7661c428920f0913
Luhan Lacerda <luhanlacerda@gmail.com>
2024-09-17 20:34:08 -0300

    infra: inclusão de terraform para subida de imagem do container para o ECR

---
+++ infra/terraform/vars.tf
@@ -0,0 +1,35 @@
+variable "projectName" {
+  default = "lanchonete-api"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    App      = "lanchonete-api",
+    Ambiente = "Desenvolvimento"
+  }
+}
+
+variable "accessKey" {
+  description = "Access Key Id"
+  type        = string
+  sensitive   = true
+}
+
+variable "secretKey" {
+  description = "Access Key Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "defaultRegion" {
+  description = "Aws Region"
+  type        = string
+  sensitive   = true
+}
+
+variable "accountId" {
+  description = "Aws Account Id"
+  type        = string
+  sensitive   = true
+}
